0c84899b1ac19bd1717575289be655bc2eb22cd9
Fictioncoding <fanoffiction616@gmail.com>
2026-07-30 23:24:57 +0530

    ..

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -55,7 +55,7 @@ export default function LoginForm() {
 
   const handleGoogleSignIn = () => {
     startGoogleTransition(() => {
-      window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL || ''}/google`;
+      window.location.href = `${process.env.NEXT_PUBLIC_API_BASE_URL}/google`;
     });
   };
 
